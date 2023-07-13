85b44c2400f951cb0bbf826c1abf151abca5afb2
JAgboola <agboola.joy@yahoo.com>
2023-07-13 18:02:42 -0400

    Models

---
+++ eTickets/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int MovieId { get; set; }
+
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public DateOnly? StartDate { get; set; }
+        public DateOnly? EndDate { get; set; }
+        public double? Price { get; set; }
+        public string? CinemaName { get; set; }
+        public string? MovieActors { get; set; }
+        public string? MovieCategory { get; set; }
+        public string? MovieProducer { get; set; }
+
+    }
+}
